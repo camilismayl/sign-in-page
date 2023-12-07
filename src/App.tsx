c776d35a7ee815b8e51e198b3d281cf9b2a5e613
camilismayl <81261829+camilismayl@users.noreply.github.com>
2023-12-07 11:47:25 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.scss';
 import router from './router/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import useGlobalStyles from 'assets/styles/style';
 import AuthComponent from 'core/layouts/auth/auth.component';
 import { RouterProvider } from 'react-router-dom';
+const queryClient = new QueryClient();
 
 function App() {
   useGlobalStyles();
   return (
-    <div className='App'>
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <RouterProvider router={router} />
+      </div>{' '}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
